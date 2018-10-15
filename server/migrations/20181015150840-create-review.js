4be0fff6d55e58cbfdf0abf63e51c17272962620
Temitope <tope_olajide@outlook.com>
2018-10-15 18:35:59 +0100

    Modify review migration file

--- server/migrations/20181015150840-create-review.js
+++ server/migrations/20181015150840-create-review.js
@@ -1,27 +1,30 @@
-'use strict';
+
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Reviews', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Reviews');
-  }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Reviews', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    content: Sequelize.STRING,
+    businessId: {
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Reviews')
 };
