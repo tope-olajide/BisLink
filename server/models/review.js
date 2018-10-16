f57c9265254320e850b9bea571fd173f725eb485
Temitope <tope_olajide@outlook.com>
2018-10-16 07:26:50 +0100

    Add foreign key to review model

--- server/models/review.js
+++ server/models/review.js
@@ -6,16 +6,34 @@
  */
 export default (sequelize, DataTypes) => {
   const Review = sequelize.define('Review', {
+    title: DataTypes.STRING,
     content: DataTypes.STRING,
     businessId: {
       type: DataTypes.INTEGER,
       onDelete: 'CASCADE',
+      references: {
+        model: 'Business',
+        key: 'id',
+        as: 'businessId',
+      }
     },
     userId: {
       type: DataTypes.INTEGER,
       onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'userId'
+      }
     }
   });
-
+  Review.associate = (models) => {
+    Review.belongsTo(models.Business, {
+      foreignKey: 'businessId',
+    });
+    Review.belongsTo(models.User, {
+      foreignKey: 'userId',
+    });
+  };
   return Review;
 };
