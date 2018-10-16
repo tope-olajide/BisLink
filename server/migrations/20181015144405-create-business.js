2f2e729fb93f558b55c8d68fb6ca6b60995e6f46
Temitope <tope_olajide@outlook.com>
2018-10-16 07:41:21 +0100

    Add foreign to business migration

--- server/migrations/20181015144405-create-business.js
+++ server/migrations/20181015144405-create-business.js
@@ -55,7 +55,14 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
-      }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        }},
     });
   },
   down: queryInterface => queryInterface.dropTable('businesses')
