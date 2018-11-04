e681f77316dd5c531b35f4151073ecf9576ddd49
Temitope <tope_olajide@outlook.com>
2018-11-04 18:07:28 +0100

    Create UserLoginForm component

--- client/src/components/presentational/LoginForm.jsx
+++ client/src/components/presentational/LoginForm.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Row, Col, Input, Button } from 'mdbreact';
-
+import './styles/LoginForm.css'
 class FormsPage extends React.Component  {
   render() {
     return(
       <Container>
         <Row>
-          <Col md="6">
-            <form>
-              <p className="h5 text-center mb-4">Sign in</p>
+          <Col md="">
+            <form className = 'form-width'>
+              <p className="h4 text-center mt-4 mb-4">Sign in</p>
               <div className="grey-text">
                 <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
                 <Input label="Type your password" icon="lock" group type="password" validate/>
