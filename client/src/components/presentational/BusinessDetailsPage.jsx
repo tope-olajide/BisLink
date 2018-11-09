b55d317b89b1d9befdc10a3d32ca6efd6ea221ff
Temitope <tope_olajide@outlook.com>
2018-11-09 13:59:32 +0100

    Modify BusinessDetailsPage component

--- client/src/components/presentational/BusinessDetailsPage.jsx
+++ client/src/components/presentational/BusinessDetailsPage.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import BusinessDetails from "./BusinessDetails";
+import BusinessSlider from "./BusinessSlider";
 class BusinessDetailsPage extends Component {
-    render () {
-        return (
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <BusinessSlider
+          img1="featured1.jpg"
+          img2="featured2.jpg"
+          img3="featured3.jpg"
+          img4="featured4.jpg"
+        />
+        <BusinessDetails />
+      </div>
+    );
+  }
 }
 export default BusinessDetailsPage;
