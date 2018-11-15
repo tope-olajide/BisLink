da897fb1b2532f03b14dbc3b7f747373d970b395
Temitope <tope_olajide@outlook.com>
2018-11-15 03:56:58 +0100

    Update App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react';
-
-// import HomeAnimation from '../src/components/presentational/HomeAnimation'
-import NavBar from '../src/components/presentational/NavBar'
-// import CataloguePage from '../src/components/presentational/CataloguePage'
-//import RegisterBusinessForm from '../src/components/presentational/RegisterBusinessForm'
-//import RegisterBusinessPage from '../src/components/presentational/RegisterBusinessPage'
-// import ProfilePage from '../src/components/presentational/ProfilePage'
-// import BusinessSlider from '../src/components/presentational/BusinessSlider'
+import { Route, Link } from 'react-router-dom';
+import NavBar from './components/presentational/NavBar'
+import HomePage from '../src/components/container/HomePage'
+import CataloguePage from '../src/components/presentational/CataloguePage'
+import RegisterBusinessPage from '../src/components/presentational/RegisterBusinessPage'
+import ProfilePage from '../src/components/presentational/ProfilePage'
 import BusinessDetailsPage from '../src/components/presentational/BusinessDetailsPage'
 class App extends Component {
   render() {
     return (
-      <div>
-       <NavBar /> 
-{/* <HomeAnimation 
-caption1 = 'Bis-Link is a direct optimized way of submitting your business details to nearby city directories.' 
-caption2 ='Search for businesses, services, houses, apartments and lands for sale or for rent'
-caption3 = 'Be visible! Obtain new customers and generate more traffic.'
-caption4 = 'Get reviews and grow your business reputation online.'
-caption5 = 'BisLink will make your website link-building strategy better...'
-caption6 = '... and imporove your online business awareness to increase your sales'
-/> */}
-{/* <CataloguePage /> */}
-{/* <RegisterBusinessPage />
-<RegisterBusinessForm /> */}
-{/* <BusinessSlider img1 = 'featured1.jpg' 
-img2 = 'featured2.jpg' 
-img3 = 'featured3.jpg' 
-img4 = 'featured4.jpg' 
-/> */}
-<BusinessDetailsPage />
+    <div>
+    <Route exact path="/" component={HomePage} />
+    <Route  path="/businesses" component={CataloguePage} />
+    <Route  path="/business/:id" component={BusinessDetailsPage} />
+    <Route  path="/register-business" component={RegisterBusinessPage} />
+    <Route  path="/profile/:userId" component={ProfilePage} />
       </div>
     );
   }
