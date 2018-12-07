1dd9822ce7734bac7ed9bf2fbab41e64636a4fbe
Temitope <tope_olajide@outlook.com>
2018-12-07 16:39:37 +0100

    Create AddBusiness.jsx

---
+++ client/src/components/container/AddBusiness.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import AddBusiness from './../presentation/RegisterBusinessPage';
+class BusinessList extends Component {
+    render () {
+        return (
+<AddBusiness />
+        )
+    }
+}
+export default BusinessList
