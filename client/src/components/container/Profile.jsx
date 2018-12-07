0414a9dcf3d5d7bf74cc1618b529a511b8ec2b95
Temitope <tope_olajide@outlook.com>
2018-12-07 17:36:13 +0100

    Create Profile.jsx

---
+++ client/src/components/container/Profile.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ProfilePage from '../presentation/ProfilePage';
+class Profile extends Component {
+    render () {
+        return (
+            <ProfilePage />
+        )
+    }
+}
+export default Profile
