eda455f62e36fc91894e292e839a7095e90cb348
Temitope <tope_olajide@outlook.com>
2018-12-07 12:46:11 +0100

    Create Home.jsx

---
+++ client/src/components/container/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import HomePage from "../presentation/HomeAnimation";
+class Home extends Component {
+  render() {
+    return (
+      <HomePage
+        caption1="Bis-Link is a direct optimized way of submitting your business details to nearby city directories."
+        caption2="Search for businesses, services, houses, apartments and lands for sale or for rent"
+        caption3="Be visible! Obtain new customers and generate more traffic."
+        caption4="Get reviews and grow your business reputation online."
+        caption5="BisLink will make your website link-building strategy better..."
+        caption6="... and imporove your online business awareness to increase your sales"
+      />
+    );
+  }
+}
+export default Home
