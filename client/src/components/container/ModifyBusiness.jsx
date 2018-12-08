6d0239e7e38bfe6b47afba990957442232f9d1d5
Temitope <tope_olajide@outlook.com>
2018-12-08 01:34:24 +0100

    Create ModifyBusiness.jsx

---
+++ client/src/components/container/ModifyBusiness.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ModifyBusinessForm from './../presentation/ModifyBusinessForm';
+class ModifyBusiness extends Component {
+    render () {
+        return (
+<ModifyBusinessForm  />
+        )
+    }
+}
+export default ModifyBusiness
