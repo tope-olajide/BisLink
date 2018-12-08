49c314efd8de700f99f2b7f16dc7f146ca84afe6
Temitope <tope_olajide@outlook.com>
2018-12-08 02:07:47 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,34 @@
 import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import {
+    BrowserRouter
+} from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faBriefcase, faSearch, faFolderPlus, faUserPlus, faSignInAlt, faMapMarker, faMobile, faLink, faHeartbeat } from '@fortawesome/free-solid-svg-icons'
+import {
+    library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+    faHome,
+    faBriefcase,
+    faSearch,
+    faFolderPlus,
+    faUserPlus,
+    faSignInAlt,
+    faMapMarker,
+    faMobile,
+    faLink,
+    faHeartbeat
+} from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-library.add(faHome, faBriefcase, faSearch, faFolderPlus, faUserPlus, faSignInAlt, faMapMarker, faMobile, faLink, faHeartbeat  )
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+library.add(faHome, faBriefcase, faSearch, faFolderPlus, faUserPlus, faSignInAlt, faMapMarker, faMobile, faLink, faHeartbeat)
+ReactDOM.render( <BrowserRouter>
+        <App/>
+        </BrowserRouter>, document.getElementById('root'));
+        registerServiceWorker();
