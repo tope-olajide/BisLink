6213b8f463160d46268da7966c1ce083d72eb8f0
Temitope <tope_olajide@outlook.com>
2018-12-11 22:21:06 +0100

    Update validator.js

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -2,8 +2,8 @@ const isAlphaNumeric = (mail) => {
   mail = mail.toString();
   const code = mail.charCodeAt(0);
   if (!(code > 47 && code < 58) && // numeric 0-9
-      !(code > 64 && code < 91) && // uppercase alphabet
-      !(code > 96 && code < 123)) { // lowercase alphabet
+    !(code > 64 && code < 91) && // uppercase alphabet
+    !(code > 96 && code < 123)) { // lowercase alphabet
     return false;
   }
   return true;
@@ -13,16 +13,16 @@ const isAlphaNumericAndSomeChar = (mail) => {
   for (let i = 0, len = mail.length; i < len; i++) {
     const code = mail.charCodeAt(i);
     if (!(code > 47 && code < 58) && // numeric 0-9
-        !(code > 64 && code < 91) && // uppercase alphabet
-        !(code === 45 || code === 46 || code === 64 || code === 95) && // Hyphen, Period, @, underscore
-        !(code > 96 && code < 123)) { // lowercase alphabet
+      !(code > 64 && code < 91) && // uppercase alphabet
+      !(code === 45 || code === 46 || code === 64 || code === 95) && // Hyphen, Period, @, underscore
+      !(code > 96 && code < 123)) { // lowercase alphabet
       return false;
     }
   }
   return true;
 };
-  // Validate Email input from user
-export const validateEmail = (emailAddress) => {
+// Validate Email input from user
+const validateEmail = (emailAddress) => {
   if (!emailAddress) {
     return false;
   }
@@ -44,15 +44,15 @@ export const validateEmail = (emailAddress) => {
   // are: "@ _ - ." and they must not follow each
   // other consecutively e.g, -- or __ or ..
   if (email[email.indexOf('.') + 1] === '.' ||
-      email[email.indexOf('_') + 1] === '_' ||
-      email[email.indexOf('-') + 1] === '-') {
+    email[email.indexOf('_') + 1] === '_' ||
+    email[email.indexOf('-') + 1] === '-') {
     return false;
   }
   // first letter in the email address must not contain special character,
   // so is the letter immdiately the @ sign and last letter
   if (isAlphaNumeric(newMailArray[0].charAt(0)) === false ||
-      isAlphaNumeric(newMailArray[1].charAt(0)) === false
-      || isAlphaNumeric(newMailArray[1][newMailArray[1].length - 1]) === false
+    isAlphaNumeric(newMailArray[1].charAt(0)) === false ||
+    isAlphaNumeric(newMailArray[1][newMailArray[1].length - 1]) === false
   ) {
     return false;
   }
@@ -60,15 +60,18 @@ export const validateEmail = (emailAddress) => {
   if (isAlphaNumericAndSomeChar(emailAddress) === false) {
     return false;
   }
-  
+
   return true;
 };
-  
-export const validateUser = (fullname, username, password) => {
-  fullname = fullname.toLowerCase();
+
+export const validateUser = ({
+  fullname,
+  username,
+  password,
+  email
+}) => {
   username = username.toLowerCase();
-  password = password.toLowerCase();
-  
+
   const checkEachChar = (char) => {
     char = parseInt(char, 10);
     return Number.isInteger(char);
@@ -78,14 +81,20 @@ export const validateUser = (fullname, username, password) => {
     const newarray = newstring.filter(checkEachChar);
     return newarray;
   };
-  
+
   const validateFullname = checkForInteger(fullname);
-  if (fullname.length < 6 ||
-      !fullname.includes(' ') || validateFullname.length > 1) {
-    return 'Enter a valid full name!';
+  if (fullname.length < 5 ||
+     validateFullname.length > 1) {
+    return 'Enter a valid fullname!';
+  }
+  if (!fullname.includes(' ')) {
+    return 'Your firstname and lastname must be separated with space';
   }
   if (username.length < 3 || username.includes(' ')) {
     return 'Username must contain at least 3 alphabets and no space!';
+  }
+  if (!validateEmail(email)) {
+    return 'please enter a valid email address'
   }
   if (password.trim().length === 0 || password.length < 5) {
     return 'Password must be at least 5 characters!';
