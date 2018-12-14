1cf7552b9ccd7dab822b27e971d9456a9a3fdcd6
Temitope <tope_olajide@outlook.com>
2018-12-14 01:01:19 +0100

    Update AddBusiness.jsx

--- client/src/components/container/AddBusiness.jsx
+++ client/src/components/container/AddBusiness.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from "react";
 import AddBusiness from './../presentation/RegisterBusinessPage';
 class BusinessList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            businessName: "",
+            tagline: "",
+            businessAddress1: "",
+            businessAddress2: "",
+            phoneNumber1: "",
+            phoneNumber2: "",
+            website: "",
+            category: "",
+            businessImageUrl: "",
+            businessImageId: "",
+            businessDescription: "",
+          files: [],
+          filesToBeSent: [],
+          imageUrl: "",
+          imageId: ""
+        };
+        this.onDrop = this.onDrop.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+      }
+      onDrop(files) {
+        this.setState({
+        filesToBeSent: files,
+          files: files.map(file => ({
+            ...file,
+            preview: URL.createObjectURL(file)
+          }))
+        });
+      }
+      handleInputChange(key, value) {
+        this.setState({ [key]: value });
+      }
     render () {
         return (
-<AddBusiness />
+<AddBusiness
+          files={this.state}
+          onDrop={this.onDrop}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+ />
         )
     }
 }
