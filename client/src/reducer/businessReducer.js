b8a528da21727b215aa870367174f6cede0e7f9a
Temitope <tope_olajide@outlook.com>
2018-12-16 00:21:17 +0100

    Update businessReducer.js

--- client/src/reducer/businessReducer.js
+++ client/src/reducer/businessReducer.js
@@ -1,4 +1,30 @@
+import {
+    ADD_BUSINESS,
+    SET_CURRENT_BUSINESS,
+    SET_MY_BUSINESSES,
+    DELETE_BUSINESS,
+    EDIT_BUSINESS
+} from '../actions/type'
+
 const initialState = {
     allBusinesses: {
-      businesses: {}
-  };
+        businesses: {}
+    },
+    myBusinesses: {
+        businesses: {}
+    },
+    currentBusiness: {}
+}
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_BUSINESS:
+        return {...state, myBusinesses: action.businesses };
+        case SET_MY_BUSINESSES:
+        return { ...state, myBusinesses: action.businesses };
+        case SET_CURRENT_BUSINESS:
+        return { ...state, currentBusiness: action.business };
+        case EDIT_BUSINESS:
+        return { ...state, myBusinesses: action.businesses };
+        default: return state;
+    }
+}
