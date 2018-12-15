0a98d1a8f5cef2fefb46d85f9a810f4deae1d335
Temitope <tope_olajide@outlook.com>
2018-12-16 00:21:31 +0100

    Update userValidation.js

--- server/middleware/userValidation.js
+++ server/middleware/userValidation.js
@@ -1,16 +1,6 @@
 import { Business } from '../models';
 
-/**
- * @description - Validate the username if taken
- *
- * @param {object} User - User model
- *
- * @param {string} username - User name
- *
- * @param {number} userId - User ID
- *
- * @returns {Promise} promise - Request status
- */
+
 export const validateUserName = (User, username, userId) => {
   const promise = new Promise((resolve, reject) => {
     User
@@ -39,19 +29,8 @@ export const validateUserName = (User, username, userId) => {
   return promise;
 };
 
-/**
- * @description - Validate if user has the right to manage recipe
- *
- * @param {string} recipeId - Recipe ID
- *
- * @param {string} userId - User ID
- *
- * @returns {Promise} promise - Status of request
- */
+
 export const validateUserRight = (businessId, userId) => { 
-// validateUserRight issa function that takes in recipeId and userId as parameters
-// it search for the recipeId in the recipe table, then compare the Id of the user that added it with the userId in the parameter
-// if the id is the same, then it returns recipe and all its object( containing userId,procedure,methods etc)
   const promise = new Promise((resolve, reject) => {
     Business
       .findById(businessId)
@@ -62,7 +41,7 @@ export const validateUserRight = (businessId, userId) => {
             message: 'You cannot modify a business not created by You!'
           });
         }
-        resolve(recipeFound);
+        resolve(businessFound);
       });
   });
   return promise;
