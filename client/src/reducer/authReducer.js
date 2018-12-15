0543eb05f374c1eb7051edc84355ce9363a7fed6
Temitope <tope_olajide@outlook.com>
2018-12-16 00:21:10 +0100

    Update authReducer.js

--- client/src/reducer/authReducer.js
+++ client/src/reducer/authReducer.js
@@ -1,13 +1,18 @@
-import  {SET_CURRENT_USER}  from './../actions/type';
+import  {SET_CURRENT_USER, SET_USER_PROFILE } from './../actions/type';
 
 const initialState = {
-  user: {}
+  user: {},
+  profile: {}
 };
 export default (state = initialState, action = {}) => {
     switch (action.type) {
       case SET_CURRENT_USER:
         return {
           ...state, user: action.userData
+        };
+        case SET_USER_PROFILE:
+        return {
+          ...state, profile: action.user
         };
       default: return state;
     }
