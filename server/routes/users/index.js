c6362ce06e1683ecd994ccb1ff62f3825168f483
Temitope <tope_olajide@outlook.com>
2018-12-16 00:22:31 +0100

    Update index.js

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import User from '../../controllers/users';
-
+import Businesses from '../../controllers/business';
+import Auth from '../../middleware/auth';
 const user = express.Router();
-
+const newBusiness = new Businesses();
 const newUser = new User();
+const newAuth = new Auth();
 
 user.post('/signup', newUser.signupUser);
 user.post('/signin', newUser.signIn);
 
+user.use('*', newAuth.verify);
+user.get('/businesses', newBusiness.getUserBusiness);
 
 /* user.route('/business/:businessId')
 .post(newFavourite.addToFavourites)
 .delete(newFavorite.removeFromFavorites);
  */
 export default user;
+
+
+
+
+
+
