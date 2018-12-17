77a06ee8c57f8cd6c8f20be84b9aa6a01d16d191
Temitope <tope_olajide@outlook.com>
2018-12-17 06:59:33 +0100

    Update index.js

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import User from '../../controllers/users';
 import Businesses from '../../controllers/business';
 import Auth from '../../middleware/auth';
+import Favourites from '../../controllers/favourite';
 const user = express.Router();
 const newBusiness = new Businesses();
 const newUser = new User();
 const newAuth = new Auth();
+const newFavourite = new Favourites();
 
 user.post('/signup', newUser.signupUser);
 user.post('/signin', newUser.signIn);
@@ -13,14 +15,11 @@ user.post('/signin', newUser.signIn);
 user.use('*', newAuth.verify);
 user.get('/businesses', newBusiness.getUserBusiness);
 
-/* user.route('/business/:businessId')
-.post(newFavourite.addToFavourites)
-.delete(newFavorite.removeFromFavorites);
- */
+user.route('/business/:businessId')
+.post(newFavourite.addToFavourite)
+.delete(newFavourite.removeFromFavourites)
+.get(newFavourite.getFavBusinesses);
+
+
 export default user;
-
-
-
-
-
 
