4e3235ffbedbf417f4e16752c87990514e9caef2
Temitope <tope_olajide@outlook.com>
2018-12-23 08:38:40 +0100

    Update index.js

--- server/routes/business/index.js
+++ server/routes/business/index.js
@@ -2,10 +2,16 @@ import express from 'express';
 
 import Business from '../../controllers/business';
 import Auth from '../../middleware/auth';
+import Reviews from '../../controllers/reviews';
+import Votes from '../../controllers/votes';
 
+const newVote = new Votes();
 const newAuth = new Auth();
+const newReview = new Reviews();
 const user = express.Router();
 const newBusiness = new Business();
+  user.route('/arraytest')
+  .get(newBusiness.arrayTest);
 
 user.use('*', newAuth.verify);
 
@@ -16,5 +22,20 @@ user.route('/')
 user.route('/:businessId')
   .put(newBusiness.modifyBusiness)
   .delete(newBusiness.deleteBusiness)
-  .get(newBusiness.getUserBusiness);
+  .get(newBusiness.getBusiness);
+
+user.route('/:businessId/upvotes')
+  .post(newVote.upvoteBusiness)
+  .get(newVote.getBusinessUpvotes);
+
+user.route('/:businessId/downvotes')
+  .post(newVote.downvoteBusiness)
+  .get(newVote.getBusinessDownvotes);
+
+user.route('/:businessId/reviews')
+  .post(newReview.postReview)
+  .get(newReview.getBusinessReviews);
+
+
+  
   export default user;
