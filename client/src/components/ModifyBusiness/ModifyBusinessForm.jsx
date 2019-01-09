1575baaaf55bd71fbfe5f50a613ea2679f1d4cab
Temitope <tope_olajide@outlook.com>
2019-01-09 14:19:55 +0100

    Update ModifyBusinessForm component

--- client/src/components/ModifyBusiness/ModifyBusinessForm.jsx
+++ client/src/components/ModifyBusiness/ModifyBusinessForm.jsx
@@ -1,69 +1,181 @@
 import React, { Component } from "react";
 import "./RegisterBusinessForm.css";
-import { Container, InputFile, Input, Button } from "mdbreact";
-class ModifyBusinessForm extends Component {
+import { Input, Button } from "mdbreact";
+import Dropzone from "react-dropzone";
+const dropZoneStyle= {
+  width:200,
+  height:200,
+  marginLeft: 'auto',
+  marginRight:'auto',
+  borderWidth: 2,
+  borderColor: '#666',
+  borderStyle: 'dashed',
+  borderRadius: 5
+}
+const thumbsContainer = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16
+};
+
+const thumb = {
+  display: "inline-flex",
+  borderRadius: 2,
+  border: "1px solid #eaeaea",
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: "border-box"
+};
+
+const thumbInner = {
+  display: "flex",
+  minWidth: 0,
+  overflow: "hidden"
+};
+
+const img = {
+  display: "block",
+  width: "auto",
+  height: "100%"
+};
+class RegisterBusinessPage extends Component {
   render() {
+    const { files } = this.props.files;
+
+    const thumbs = files.map(file => (
+      <div style={thumb}>
+        <div style={thumbInner}>
+          <img src={file.preview} style={img} />
+        </div>
+      </div>
+    ));
     return (
-      <div className="biz-form-container card p-5">
-        <h5 className="dark-text biz-form-title  text-center ">
-          Modify my business
-        </h5>
-        <form>
-          <div className="row">
-            <div className="col-md-6">
-              <Input label="Business name" icon="building" />
-            </div>
-            <div className="col-md-6">
-              <Input label="Tag Line" icon="building" />
-            </div>
+      <div>
+        <div className="register-business-header">
+          <div className="hero-text">
+            <h1>
+             Modify Business
+            </h1>
           </div>
-          <Input label="Address" icon="building" />
-          <Input label=" Address 2" className="mt-5" icon="building" />
-          <div className="row">
-            <div className="col-md-6">
-              <Input label="Phone Number" icon="building" />
-            </div>
-            <div className="col-md-6">
-              <Input label="Phone Number 2" icon="building" />
-            </div>
+        </div>
+        <div className="biz-form-container mt-5">
+                <div className="  text-right ">
+             <Button onClick={this.props.handleFormSubmit} >
+                  Modify Picture Gallery
+                </Button>
           </div>
-          <div className="row">
-            <div className="col-md-6">
-              <Input label="Email Address" icon="building" />
-            </div>
-            <div className="col-md-6">
-              <Input label="Category" icon="building" />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <Input label="Website" icon="building" />
-            </div>
-            <div className="col-md-6">
-              <div class="form-group">
-                <label for="exampleInputFile" class="bmd-label-floating">
-                  Upload your business Pictures
-                </label>
-                <input
-                  type="file"
-                  class="form-control-file"
-                  id="exampleInputFile"
+        </div>
+
+        <div className="biz-form-container card  p-5">
+          <form>
+            <div className="row">
+              <div className="col-md-6">
+                <Input
+                  label="Business name"
+                  icon="building"
+                  onChange={event => {
+                    this.props.handleInputChange(
+                      "businessName",
+                      event.target.value
+                    );
+                  }}
                 />
-                <small class="text-muted">Maximum of 10</small>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  label="Tag Line"
+                  icon="building"
+                  onChange={event => {
+                    this.props.handleInputChange(
+                      "tagline",
+                      event.target.value
+                    );
+                  }}
+                />
               </div>
             </div>
-          </div>
+            <Input
+              label="Email Address"
+              icon="building"
+              onChange={event => {
+                this.props.handleInputChange("email", event.target.value);
+              }}
+            />
+            <Input
+              label="Business Address"
+              className="mt-5"
+              icon="building"
+              onChange={event => {
+                this.props.handleInputChange("businessAddress1", event.target.value);
+              }}
+            />
+            <div className="row">
+              <div className="col-md-6">
+                <Input
+                  label="Phone Number"
+                  icon="building"
+                  onChange={event => {
+                    this.props.handleInputChange(
+                      "phoneNumber1",
+                      event.target.value
+                    );
+                  }}
+                />
+              </div>
+              <div className="col-md-6">
+                <Input
+                  label="Business Category"
+                  icon="building"
+                  onChange={event => {
+                    this.props.handleInputChange(
+                      "category",
+                      event.target.value
+                    );
+                  }}
+                />
+              </div>
+            </div>
 
-          <Input
-            type="textarea"
-            label="Business descriptions"
-            icon="pencil"
-            rows="3"
-          />
-        </form>
+            <Input
+              label="Website"
+              icon="building"
+              onChange={event => {
+                this.props.handleInputChange("website", event.target.value);
+              }}
+            />
+            <Input
+              type="textarea"
+              label="Business descriptions"
+              icon="pencil"
+              rows="3"
+              onChange={event => {
+                this.props.handleInputChange("fullname", event.target.value);
+              }}
+            />
+            <section>
+              <div className="dropzone">
+                <Dropzone accept="image/*" style={dropZoneStyle} onDrop={this.props.onDrop}>
+                  <h4>
+                    Try dropping your picture here, or click to select the
+                    picture you want to upload.
+                  </h4>
+                </Dropzone>
+                <aside style={thumbsContainer}>{thumbs}</aside>
+              </div>
+              <div className="text-center">
+                <Button onClick={this.props.handleFormSubmit} >
+                  Update Business
+                </Button>
+              </div>
+            </section>
+          </form>
+        </div>
       </div>
     );
   }
 }
-
-export default ModifyBusinessForm;
+export default RegisterBusinessPage;
--- client/src/components/ModifyBusiness/index.jsx
+++ client/src/components/ModifyBusiness/index.jsx
@@ -1,9 +1,101 @@
 import React, { Component } from "react";
-import ModifyBusinessForm from './../presentation/ModifyBusinessForm';
+import axios from 'axios'
+import ModifyBusinessForm from './ModifyBusinessForm';
+import NavBar from './../commons/NavigationBar';
 class ModifyBusiness extends Component {
+    constructor() {
+        super();
+        this.state = {
+            businessName: "",
+            tagline: "",
+            businessAddress1: "",
+            phoneNumber1: "",
+          website: "",
+          category: "",
+          businessImageUrl: "",
+          businessImageId: "",
+          businessDescription: "",
+          files: [],
+          filesToBeSent: [],
+          imageUrl: "",
+          imageId: ""
+        };
+        this.onDrop = this.onDrop.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+      }
+      onDrop(files) {
+        this.setState({
+        filesToBeSent: files,
+          files: files.map(file => ({
+            ...file,
+            preview: URL.createObjectURL(file)
+          }))
+        });
+      }
+      handleInputChange(key, value) {
+        this.setState({ [key]: value });
+      }
+      handleFormSubmit = files => {
+        alert("Loading....");
+        // start loading animation
+        // Push all the axios request promise into a single array
+        const uploaders = this.state.filesToBeSent.map( file => {
+          console.log(file);
+          // Initial FormData
+          const formData = new FormData();
+    
+          formData.append("upload_preset", "sijxpjkn");
+          formData.append("api_key", "139423638121511");
+          formData.append("file", file);
+          formData.append("timestamp", (Date.now() / 1000) | 0);
+    
+          // Make an AJAX upload request using Axios
+          return axios
+            .post(
+              "https://api.cloudinary.com/v1_1/temitope/image/upload",
+              formData,
+              {
+                headers: { "X-Requested-With": "XMLHttpRequest" }
+              }
+            )
+            .then(response => {
+              const data = response.data;
+              this.setState({
+                imageUrl: data.secure_url,
+                imageId: data.public_id
+              });
+              console.log(data);
+            })
+            .catch(function(err) {
+              alert("error " + err);
+            });
+        });
+    
+        // Once all the files are uploaded
+        axios
+          .all(uploaders)
+          .then(data => {
+            alert(
+              "Success!!!: Upload picture successfully, now saving to database"
+            );
+            this.props.onAddUser(this.state);
+            alert("saved to database successfully");
+            
+          })
+          .catch(function(err) {
+            alert(err);
+          });
+      };
     render () {
         return (
-<ModifyBusinessForm  />
+          <div>
+          <NavBar business = 'active'/>
+          <ModifyBusinessForm
+          files={this.state}
+          onDrop={this.onDrop}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+ /></div>
         )
     }
 }
