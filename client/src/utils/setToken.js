ed1d9aa938a70fe1b44d686a8ef3d4976b4a65ad
Temitope <tope_olajide@outlook.com>
2019-02-01 17:13:46 +0100

    Update setToken.js

--- client/src/utils/setToken.js
+++ client/src/utils/setToken.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-
-export default function setToken(token) {
+export default setToken=(token) => {
   if (token) {
     axios.defaults.headers.common['authorization'] = token;
-    console.log(token)
   } else {
     delete axios.defaults.headers.common['authorization'];
   }
