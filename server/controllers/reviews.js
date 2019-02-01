90c0c1faf6b6bf8a45c09bba2ee9123ae7275036
Temitope <tope_olajide@outlook.com>
2019-02-01 17:12:37 +0100

    Update Review Controller.js

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,98 +1,127 @@
-import {Review, Business,Notification, User } from '../models';
-import { validateReview } from '../middleware/validator';
+import {
+  Review,
+  Business,
+  Notification,
+  User
+} from '../models';
+import {
+  validateReview
+} from '../middleware/validator';
 
 export default class Reviews {
-    postReview({
-        user,
-        params,
-        body
-    }, res) {
-        const userId = user.id;
-        const {
-            businessId
-        } = params;
-        const title = body.title;
-        const content = body.content
-        const validateReviewError = validateReview({
-            title,
-            content
-        });
-        if (validateReviewError) {
-            return res.status(400).json({
-                success: false,
-                message: validateReviewError
-            });
-        }
-        Review
-            .create({
-                title,
-                content,
-                userId,
-                businessId
-            })
-            .then((createdReview) => {
-              Business
-              .findOne({
-                where: { id: businessId }
-              }).then((business) =>{         
-    const notificationAlert = {
-      receiverId:business.userId,
-      title:`${user.username} has reviewed one of your businesses`,
-      message: `${user.username} has added a review to one of your business titled: '${business.businessName}'`
-    };
-    Notification
-              .create({
-                userId:notificationAlert.receiverId,
-                title:notificationAlert.title,
-                message:notificationAlert.message,
-              })
-              })
-                const {id, title, content, createdAt } = createdReview;
-                const review = { id,title, content, createdAt }
-                return res.status(201).json({
-                    success: true,
-                    message: 'New review created',
-                    review,
-                    reviewer: user.username
-                  })
-            }).catch((error) => res.status(500).json({
-              success: false,
-              message: 'Error Creating review',
-              error
-            }));
+  postReview({
+    user,
+    params,
+    body
+  }, res) {
+    const userId = user.id;
+    const {
+      businessId
+    } = params;
+    const title = body.title;
+    const content = body.content
+    const validateReviewError = validateReview({
+      title,
+      content
+    });
+    if (validateReviewError) {
+      return res.status(400).json({
+        success: false,
+        message: validateReviewError
+      });
     }
-    getBusinessReviews({ params }, res) {
-        const businessId = params.businessId;
-        Review
-          .findAll({
-            where: { businessId },
-            include: [
-              { model: User, attributes: ['username','ImageUrl'] }
-            ],
+    Review
+      .create({
+          title,
+          content,
+          userId,
+          businessId
+        }
+
+      )
+      .then((createdReview) => {
+        Business
+          .findOne({
+            where: {
+              id: businessId
+            }
+          }).then((business) => {
+            const notificationAlert = {
+              receiverId: business.userId,
+              title: `${user.username} has reviewed one of your businesses`,
+              message: `${user.username} has added a review to one of your business titled: '${business.businessName}'`
+            };
+            Notification
+              .create({
+                userId: notificationAlert.receiverId,
+                title: notificationAlert.title,
+                message: notificationAlert.message,
+              })
+          })
+          Review
+          .findOne({
+            where: {
+              id:createdReview.id
+            },
+            include: [{
+              model: User,
+              attributes: ['username', 'ImageUrl']
+            }],
             order: [
               ['id', 'DESC']
             ]
+          }).then((reviews)=>{
+          return res.status(201).json({
+          success: true,
+          message: 'New review created',
+          reviews: reviews,
+          reviewer: createdReview.id
+        })
           })
-          .then((reviews) => {
-            if (reviews.length === 0) {
-              return res.status(200).json({
-                success: true,
-                message: 'No Review found!',
-                reviews: []
-              });
-            }
-            return res.status(200).json({
-              success: true,
-              message: 'Review(s) found',
-              reviews
-            });
-          })
-          .catch((error) => res.status(500).json({
-            success: false,
-            message: 'Error fetching reviews',
-            error
-          }));
-    
-        return this;
-      }
+
+      }).catch((error) => res.status(500).json({
+        success: false,
+        message: 'Error Creating review',
+        error
+      }));
+  }
+  getBusinessReviews({
+    params
+  }, res) {
+    const businessId = params.businessId;
+    Review
+      .findAll({
+        where: {
+          businessId
+        },
+        include: [{
+          model: User,
+          attributes: ['username', 'ImageUrl']
+        }],
+        order: [
+          ['id', 'DESC']
+        ]
+      })
+      .then((reviews) => {
+        if (reviews.length === 0) {
+          return res.status(200).json({
+            success: true,
+            message: 'No Review found!',
+            reviews: []
+          });
+        }
+        return res.status(200).json({
+          success: true,
+          message: 'Review(s) found',
+          reviews
+        });
+      })
+      .catch((error) => res.status(500).json({
+        success: false,
+        message: 'Error fetching reviews',
+        error
+      }));
+
+    return this;
+  }
 }
