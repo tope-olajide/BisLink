ad52313aeb2d941cc7d63269312875f78ff44370
Temitope <tope_olajide@outlook.com>
2019-02-01 17:16:48 +0100

    Add reviewReducer

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -5,6 +5,7 @@ import businessReducer from './businessReducer';
 import userReducer from './userReducer';
 import notificationsReducer from './notificationsReducer';
 import favouriteReducer from './favouriteReducer'
+import reviewReducer from './reviewReducer'
 export default combineReducers({
-    authReducer, businessReducer,userReducer,notificationsReducer,favouriteReducer
+    authReducer, businessReducer,userReducer,notificationsReducer,favouriteReducer,reviewReducer
 });
