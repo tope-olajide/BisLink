9f44acefb47016621f5c7b0a7d69ffe1fbd190d6
Temitope <tope_olajide@outlook.com>
2019-02-14 20:02:41 +0100

    Update notificationAction.js

--- client/src/actions/notificationAction.js
+++ client/src/actions/notificationAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {
-NEW_NOTIFICATIONS,NOTIFICATION_DETAILS,ALL_NOTIFICATIONS
+NEW_NOTIFICATIONS,NOTIFICATION_DETAILS,ALL_NOTIFICATIONS,READ_NOTIFICATIONS
 } from '../actions/type'
 const url = 'http://127.0.0.1:5000/api/user/notifications'
 
@@ -8,12 +8,25 @@ export function fetchAllNewNotifications() {
     return dispatch => axios.get(`${url}`)
         .then((response) => {
             const {
-                unreadNotifications,allNotificationsCount
+                unreadNotifications,allNotificationsCount,readNotificationsCount
             } = response.data;
-            const unreadNotificationsPlusCount = {unreadNotifications,allNotificationsCount }
+            const unreadNotificationsPlusCount = {unreadNotifications,allNotificationsCount,readNotificationsCount }
             dispatch({
                 type: NEW_NOTIFICATIONS,
                 unreadNotificationsPlusCount
+            });
+        });
+}
+export function fetchAllReadNotifications() {
+    return dispatch => axios.get(`${url}/seen`)
+        .then((response) => {
+            const {
+                unreadNotificationsCount,allNotificationsCount,readNotifications
+            } = response.data;
+            const readNotificationsPlusCount = {unreadNotificationsCount,allNotificationsCount,readNotifications }
+            dispatch({
+                type: READ_NOTIFICATIONS,
+                readNotificationsPlusCount
             });
         });
 }
@@ -21,11 +34,12 @@ export function fetchSingleNotification(notificationId) {
     return dispatch => axios.get(`${url}/${notificationId}`)
         .then((response) => {
             const {
-                notification
+                notification,unreadNotificationsCount,allNotificationsCount,readNotificationsCount
             } = response.data;
+            const notificationsDetailsPlusCount = {notification,unreadNotificationsCount,allNotificationsCount,readNotificationsCount}
             dispatch({
                 type: NOTIFICATION_DETAILS,
-                notification
+                notificationsDetailsPlusCount
             });
         });
 }
@@ -33,9 +47,9 @@ export function fetchAllNotifications() {
     return dispatch => axios.get(`${url}/all`)
         .then((response) => {
             const {
-                allNotifications,newNotificationsCount
+                allNotifications,newNotificationsCount,readNotificationsCount
             } = response.data;
-         const  allNotificationsPlusCount = {allNotifications, newNotificationsCount}
+         const  allNotificationsPlusCount = {allNotifications, newNotificationsCount,readNotificationsCount}
             dispatch({
                 type: ALL_NOTIFICATIONS,
                 allNotificationsPlusCount
