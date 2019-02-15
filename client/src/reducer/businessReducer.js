9fce1a07fc2d89f094ed3230a59c5a5a90573aec
Temitope <tope_olajide@outlook.com>
2019-02-15 20:17:44 +0100

    Update businessReducer.js

--- client/src/reducer/businessReducer.js
+++ client/src/reducer/businessReducer.js
@@ -1,90 +1,127 @@
 import {
-    ADD_BUSINESS,
-    SET_BUSINESS_DETAILS,
-    FETCH_ALL_BUSINESSES,
-    FETCH_BUSINESS_REVIEWS,
-    ADD_BUSINESS_REVIEW,
-    UPVOTE_BUSINESS,
-    DOWNVOTE_BUSINESS,
-    MODIFY_BUSINESS,FOLLOW_USER,UNFOLLOW_USER
+  ADD_BUSINESS,
+  SET_BUSINESS_DETAILS,
+  FETCH_ALL_BUSINESSES,
+  REMOVE_FROM_FAVOURITE,
+  ADD_TO_FAVOURITE,
+  UPVOTE_BUSINESS,
+  DOWNVOTE_BUSINESS,
+  MODIFY_BUSINESS,
+  FOLLOW_USER,
+  UNFOLLOW_USER
 } from '../actions/type'
 
 const initialState = {
-    allBusinesses: {
-        businesses: {}
-    },
-    myBusinesses: {
-        businesses: {}
-    },
-    businessDetails: {
-        
-    },
-    fetchBusinessReviews:{
-       
-    },
-    addBusinessReview:{
-        
-    }
+  allBusinesses: {
+    businesses: {}
+  },
+  myBusinesses: {
+    businesses: {}
+  },
+  businessDetails: {
+
+  },
+  fetchBusinessReviews: {
+
+  },
+  addBusinessReview: {
+
+  }
 }
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case FETCH_ALL_BUSINESSES:
-        return {...state, allBusinesses: action.pagedBusiness }
-        case SET_BUSINESS_DETAILS:
-        return { ...state, businessDetails: action.businessDetails };
-        case UPVOTE_BUSINESS:
-        return {
-          ...state,
-          businessDetails: {
-            ...state.businessDetails,
-              business:{
-                  ...state.businessDetails.business,
-                  upvotes: action.business.upvotes,
-                  downvotes: action.business.downvotes                 
-              }
+  switch (action.type) {
+    case FETCH_ALL_BUSINESSES:
+      return {
+        ...state,
+        allBusinesses: action.pagedBusiness
+      }
+    case SET_BUSINESS_DETAILS:
+      return {
+        ...state,
+        businessDetails: action.businessDetails
+      };
+    case UPVOTE_BUSINESS:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          business: {
+            ...state.businessDetails.business,
+            upvotes: action.business.upvotes,
+            downvotes: action.business.downvotes
           }
-        };
-        case DOWNVOTE_BUSINESS:
-        return {
-          ...state,
-          businessDetails: {
-            ...state.businessDetails,
-              business:{
-                  ...state.businessDetails.business,
-                  upvotes: action.business.upvotes,
-                  downvotes: action.business.downvotes                 
-              }
+        }
+      };
+    case DOWNVOTE_BUSINESS:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          business: {
+            ...state.businessDetails.business,
+            upvotes: action.business.upvotes,
+            downvotes: action.business.downvotes
           }
-        };
-        case FOLLOW_USER:
-        return {
-          ...state,
-          businessDetails: {
-            ...state.businessDetails,
-              infoCount:{
-                  ...state.businessDetails.infoCount,
-                  isFollowing: action.isFollowing,
-                  followersCount: state.businessDetails.infoCount.followersCount+1             
-              }
+        }
+      };
+    case FOLLOW_USER:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isFollowing: action.isFollowing,
+            followersCount: state.businessDetails.infoCount.followersCount + 1
           }
-        };
-        case UNFOLLOW_USER:
-        return {
-          ...state,
-          businessDetails: {
-            ...state.businessDetails,
-              infoCount:{
-                  ...state.businessDetails.infoCount,
-                  isFollowing: action.isFollowing,
-                  followersCount: state.businessDetails.infoCount.followersCount-1              
-              }
+        }
+      };
+    case UNFOLLOW_USER:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isFollowing: action.isFollowing,
+            followersCount: state.businessDetails.infoCount.followersCount - 1
           }
-        };
-        case ADD_BUSINESS:
-        return {...state, [action.business.id]: action.business };
-        case MODIFY_BUSINESS:
-        return {...state, [action.business.id]: action.business };
+        }
+      };
+    case ADD_TO_FAVOURITE:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isUserFavourite: action.isFavourite
+          }
+        }
+      }
+      case REMOVE_FROM_FAVOURITE:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isUserFavourite: action.isFavourite
+          }
+        }
+      }
+    case ADD_BUSINESS:
+      return {
+        ...state,
+        [action.business.id]: action.business
+      };
+    case MODIFY_BUSINESS:
+      return {
+        ...state,
+        [action.business.id]: action.business
+      };
 
-        default: return state;
-    }
+    default:
+      return state;
+  }
 }
--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -4,9 +4,8 @@ import authReducer from './authReducer';
 import businessReducer from './businessReducer';
 import userReducer from './userReducer';
 import notificationsReducer from './notificationsReducer';
-import favouriteReducer from './favouriteReducer'
 import reviewReducer from './reviewReducer'
 import galleryReducer from './galleryReducer'
 export default combineReducers({
-    authReducer, businessReducer,userReducer,notificationsReducer,favouriteReducer,reviewReducer,galleryReducer
+    authReducer, businessReducer,userReducer,notificationsReducer,reviewReducer,galleryReducer
 });
