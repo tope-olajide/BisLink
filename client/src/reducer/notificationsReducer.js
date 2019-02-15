37396dea6294cb5949a529d0d18a4fc2a0912405
Temitope <tope_olajide@outlook.com>
2019-02-15 20:18:33 +0100

    Update Notifcation reducer

--- client/src/reducer/notificationsReducer.js
+++ client/src/reducer/notificationsReducer.js
@@ -1,11 +1,14 @@
 import {
-    NEW_NOTIFICATIONS,NOTIFICATION_DETAILS,ALL_NOTIFICATIONS
+    NEW_NOTIFICATIONS,NOTIFICATION_DETAILS,ALL_NOTIFICATIONS,READ_NOTIFICATIONS
     } from '../actions/type'
     const initialState = {
         allNotifications: {
             notifications: {}
         },
         unreadNotification: {
+            notifications: {}
+        },
+        readNotification: {
             notifications: {}
         },
         viewNotificationDetails:{
@@ -16,8 +19,10 @@ import {
         switch (action.type) {
             case NEW_NOTIFICATIONS:
             return {...state, unreadNotification: action.unreadNotificationsPlusCount }
+            case READ_NOTIFICATIONS:
+            return {...state, readNotification: action.readNotificationsPlusCount }
             case NOTIFICATION_DETAILS:
-            return { ...state, viewNotificationDetails: action.notification };
+            return { ...state, viewNotificationDetails: action.notificationsDetailsPlusCount };
             case ALL_NOTIFICATIONS:
             return { ...state, allNotifications: action.allNotificationsPlusCount };
             default: return state;
