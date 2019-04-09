024d0abe47bdd944e0e5fb52aad076b4382c13b4
Temitope <tope_olajide@outlook.com>
2019-04-09 15:42:42 +0100

    Create .sequelizerc and update config.ja

--- server/config/config.js
+++ server/config/config.js
@@ -1,27 +1,5 @@
 require('dotenv').config();
-/* {
-  "development": {
-      "username": "Temitope",
-      "password": "liverp00l",
-      "database": "Bislink",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
-    },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-} */
+
 module.exports = {
   development: {
     username: process.env.username,
@@ -29,9 +7,6 @@ module.exports = {
     database: process.env.database,
     host: process.env.host,
     dialect: process.env.dialect
-  },
-  test: {
-    use_env_variable: 'DATABASE_URL_TEST'
   },
   production: {
     use_env_variable: 'DATABASE_URL'
