66f7e255148665d746b722fe0cc306cf2db94c7e
Temitope <tope_olajide@outlook.com>
2019-04-10 16:12:55 +0100

    Install font-awesome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -75,65 +75,69 @@ library.add(
 );
 class App extends Component {
   render() {
-    return (<div>
-      <Switch>
-        <Route exact path="/auth" component={AuthPage} />
-        <Route
-          exact
-          path="/register-business"
-          component={withAuthorization(AddBusiness)}
-        />
-        <Route exact path="/" component={withAuthorization(BusinessList)} />
-        <Route
-          path="/business-details/:businessId"
-          component={withAuthorization(BusinessDetails)}
-        />
-        <Route
-          exact
-          path="/view-profile"
-          component={withAuthorization(Profile)}
-        />
-        <Route path="/modify-user" component={withAuthorization(ModifyUser)} />
-        <Route
-          path="/modify-business/:businessId"
-          component={withAuthorization(ModifyBusiness)}
-        />
-        <Route
-          exact
-          path="/notifications"
-          component={withAuthorization(Notifications)}
-        />
-        <Route
-          exact
-          path="/notifications/all"
-          component={withAuthorization(AllNotifications)}
-        />
-        <Route
-          exact
-          path="/notifications/seen"
-          component={withAuthorization(SeenNotification)}
-        />
-        <Route
-          exact
-          path="/notifications/:notificationId"
-          component={withAuthorization(NotificationDetails)}
-        />
-        <Route
-          exact
-          path="/modify-gallery/:businessId"
-          component={withAuthorization(ModifyGallery)}
-        />
-        <Route
-          exact
-          path="/businesses/search/name=:name?/location=:location?"
-          component={withAuthorization(BusinessSearch)}
-        />
-        <Route
-          exact
-          path="/businesses/search/sort=:sort?"
-          component={withAuthorization(BusinessSearch)}
-        />
-      </Switch>
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/auth" component={AuthPage} />
+          <Route
+            exact
+            path="/register-business"
+            component={withAuthorization(AddBusiness)}
+          />
+          <Route exact path="/" component={withAuthorization(BusinessList)} />
+          <Route
+            path="/business-details/:businessId"
+            component={withAuthorization(BusinessDetails)}
+          />
+          <Route
+            exact
+            path="/view-profile"
+            component={withAuthorization(Profile)}
+          />
+          <Route
+            path="/modify-user"
+            component={withAuthorization(ModifyUser)}
+          />
+          <Route
+            path="/modify-business/:businessId"
+            component={withAuthorization(ModifyBusiness)}
+          />
+          <Route
+            exact
+            path="/notifications"
+            component={withAuthorization(Notifications)}
+          />
+          <Route
+            exact
+            path="/notifications/all"
+            component={withAuthorization(AllNotifications)}
+          />
+          <Route
+            exact
+            path="/notifications/seen"
+            component={withAuthorization(SeenNotification)}
+          />
+          <Route
+            exact
+            path="/notifications/:notificationId"
+            component={withAuthorization(NotificationDetails)}
+          />
+          <Route
+            exact
+            path="/modify-gallery/:businessId"
+            component={withAuthorization(ModifyGallery)}
+          />
+          <Route
+            exact
+            path="/businesses/search/name=:name?/location=:location?"
+            component={withAuthorization(BusinessSearch)}
+          />
+          <Route
+            exact
+            path="/businesses/search/sort=:sort?"
+            component={withAuthorization(BusinessSearch)}
+          />
+        </Switch>
       </div>
     );
   }
