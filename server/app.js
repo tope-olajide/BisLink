67b0aa3b15afbd3d9d9f966cd5bb3deca14c7a8e
Temitope <tope_olajide@outlook.com>
2019-04-16 13:20:25 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,32 +1,42 @@
-import routes from './routes';
 import cors from 'cors';
-import path from 'path';
+import routes from './routes';
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const app = express();
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build'));
-});
-
-
-
+const app = express()
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(cors());
+app.get('/', (req, res) => {
+  res.status(201).json({
+    title: 'BisLink',
+    message: 'Welcome to bisLink Homepage!'
+  });
+});
 app.use('/api/', routes);
 
-app.use('/', express.static('build'));
-app.use('*', express.static('build'));
+app.get('*', (req, res) => {
+  res.status(404).send({
+    success: false,
+    message: 'invalid link'
+  });
+});
 
-  
- const port = process.env.PORT || 5000;
+app.post('*', (req, res) => {
+  res.status(404).send({
+    success: false,
+    message: 'invalid link'
+  });
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up and running on port:', ${port}`);
 });
