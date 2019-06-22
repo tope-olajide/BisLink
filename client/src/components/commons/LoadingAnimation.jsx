05ae4d8f6f3738272e6185dba38904f35b4e1867
Temitope <tope_olajide@outlook.com>
2019-06-22 02:17:15 +0100

    Update LoadingAnimation.jsx

--- client/src/components/commons/LoadingAnimation.jsx
+++ client/src/components/commons/LoadingAnimation.jsx
@@ -1,15 +1,12 @@
-import React, {Component} from 'react'
-import './loadingAnimation.css'
+import React, { Component } from "react";
+import "./loadingAnimation.css";
 class LoadingAnimation extends Component {
-    render () {
-        return (
-            <div className = 'loading-container'>
-            <img
-            className="loader"
-            src="../loader.gif"
-            alt=""
-          />
-        </div>)
-    }
+  render() {
+    return (
+      <div className="loading-container">
+        <img className="loader" src="../loader.gif" alt="" />
+      </div>
+    );
+  }
 }
-export default LoadingAnimation
+export default LoadingAnimation;
