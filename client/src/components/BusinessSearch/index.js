6fc1804e7b9a528312d40e267963ebd0a9865b2a
Temitope <tope_olajide@outlook.com>
2019-06-24 19:06:23 +0100

    Update BusinessSearch Component

--- client/src/components/BusinessSearch/index.js
+++ client/src/components/BusinessSearch/index.js
@@ -1,14 +1,15 @@
-import React, {Component} from "react";
-import BusinessCard from '../commons/BusinessCard'
+import React, { Component } from "react";
+import BusinessCard from "../commons/BusinessCard";
 import NavigationBar from "../commons/NavigationBar";
 import Footer from "../commons/Footer";
 import toastNotification from "./../../utils/toastNotification";
 import LoadingAnimation from "../commons/LoadingAnimation";
-import { businessSearch,sortBusinessBy } from "../../actions/businessActions";
+import { businessSearch, sortBusinessBy } from "../../actions/businessActions";
 import { connect } from "react-redux";
 import Pagination from "react-js-pagination";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import CataloguePageHeader from './../Catalogue/CataloguePageHeader'
+import CataloguePageHeader from "./../Catalogue/CataloguePageHeader";
+import ErrorPage from "../commons/ErrorPage";
 class searchBusiness extends Component {
   constructor(props) {
     super(props);
@@ -19,97 +20,90 @@ class searchBusiness extends Component {
     };
   }
   componentDidMount() {
-
-    this.handlePageChange()
+    this.handlePageChange();
   }
 
-  handlePageChange = (pageNumber) => {
-    const {sort} = this.props.match.params;
-    console.log(`active page is ${pageNumber}`);
-    this.setState({activePage: pageNumber});
+  handlePageChange = pageNumber => {
+    const { sort } = this.props.match.params;
+    this.setState({ activePage: pageNumber });
     this.setState({ isLoading: true });
-    this.searchOrSort(sort)
-
-  }
-   searchOrSort =(sort) =>{
-    const {name} = this.props.match.params;
-    const {location} = this.props.match.params;
-    const limit = 9
-    if(sort){
+    this.searchOrSort(sort);
+  };
+  searchOrSort = sort => {
+    const { name } = this.props.match.params;
+    const { location } = this.props.match.params;
+    const limit = 9;
+    if (sort) {
       this.props
-      .dispatch(sortBusinessBy(sort, this.state.activePage, limit))
-      .then(() => {
-        this.setState({
-          isLoading: false,
-          isError: false
+        .dispatch(sortBusinessBy(sort, this.state.activePage, limit))
+        .then(() => {
+          this.setState({
+            isLoading: false,
+            isError: false
+          });
+        })
+        .catch(error => {
+          this.setState({
+            isLoading: false,
+            isError: true
+          });
+          if (!error.response) {
+            toastNotification(["error"], "Network Error!");
+          } else {
+            toastNotification(["error"], error.response.data.message);
+          }
         });
-      })
-      .catch(error => {
-        this.setState({
-          isLoading: false,
-          isError: true
-        });
-        if (!error.response){
-            toastNotification(["error"],'Network Error!' )
-        }else {
-             toastNotification(["error"], error.response.data.message);
-        }
-       
-      });
-    }
-    else{
+    } else {
       this.props
-      .dispatch(businessSearch(name, location, this.state.activePage, limit))
-      .then(() => {
-        this.setState({
-          isLoading: false,
-          isError: false
+        .dispatch(businessSearch(name, location, this.state.activePage, limit))
+        .then(() => {
+          this.setState({
+            isLoading: false,
+            isError: false
+          });
+        })
+        .catch(error => {
+          this.setState({
+            isLoading: false,
+            isError: true
+          });
+          if (!error.response) {
+            toastNotification(["error"], "Network Error!");
+          } else {
+            toastNotification(["error"], error.response.data.message);
+          }
         });
-      })
-      .catch(error => {
-        this.setState({
-          isLoading: false,
-          isError: true
-        });
-        if (!error.response){
-            toastNotification(["error"],'Network Error!' )
-        }else {
-             toastNotification(["error"], error.response.data.message);
-        }
-       
-      });
     }
-   }
+  };
   render() {
     if (this.state.isLoading) {
       return (
-        <div>
-          <NavigationBar search ="active" />
+        <>
           <LoadingAnimation />
-        </div>
+        </>
       );
     } else if (this.state.isError) {
       return (
-        <div>
-          <NavigationBar search ="active"/>
-          <h1>An Error has occured</h1>
-        </div>
-      );
-    }else {
-      return (
         <>
-          <NavigationBar search ="active" />
-          <CataloguePageHeader />
-          <h1 className="text-center my-5 featured-text">
-          <FontAwesomeIcon icon="search" /> Search Results </h1>
-          <div className="container content-container">
-            <div className="row card-container">
-              {!this.props.allBusinesses.length ? (
-                <div>No Business</div>
-              ) : (
-                this.props.allBusinesses.map(business => {
-                  return (
-                    <>
+          <ErrorPage />
+        </>
+      );
+    }
+    return (
+      <>
+        <NavigationBar search="active" />
+        <CataloguePageHeader />
+        <h1 className="text-center my-5 featured-text">
+          <FontAwesomeIcon icon="search" /> Search Results{" "}
+        </h1>
+        <div className="container content-container">
+          <div className="row card-container">
+            {!this.props.allBusinesses.length ? (
+              <div>No Business</div>
+            ) : (
+              this.props.allBusinesses.map(business => {
+                return (
+                  <>
                     <BusinessCard
                       key={business.id}
                       id={business.id}
@@ -121,44 +115,40 @@ class searchBusiness extends Component {
                       website={business.website}
                       image={business.defaultBusinessImageUrl}
                       viewCount={business.viewCount}
-                       upvotes={business.upvotes}
+                      upvotes={business.upvotes}
                       downvotes={business.downvotes}
                     />
-
-                    </>
-                  );
-                })
-              )}
-
-            </div>
-            <div className =''> 
-            <Pagination
-          activePage={this.state.activePage}
-          itemsCountPerPage={9}
-          totalItemsCount={this.props.totalPages}
-          pageRangeDisplayed={5}
-          onChange={this.handlePageChange}
-          innerClass={'pagination pagination-lg'}
-          itemClass = {'page-item'}
-          linkClass={'page-link'}
-          disabledClass={'disabled'}
-          activeLinkClass={'active'}
-          activeClass={'active'}
-        /></div>
+                  </>
+                );
+              })
+            )}
           </div>
-<Footer />
-        </>
-      );
-    }
-
+          <div className="">
+            <Pagination
+              activePage={this.state.activePage}
+              itemsCountPerPage={9}
+              totalItemsCount={this.props.totalPages}
+              pageRangeDisplayed={5}
+              onChange={this.handlePageChange}
+              innerClass={"pagination pagination-lg"}
+              itemClass={"page-item"}
+              linkClass={"page-link"}
+              disabledClass={"disabled"}
+              activeLinkClass={"active"}
+              activeClass={"active"}
+            />
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
   }
 }
 const mapStateToProps = state => {
-  console.log(state.businessReducer)
-return {
-  allBusinesses: state.businessReducer.allBusinesses.businesses,
-  totalPages:state.businessReducer.allBusinesses.totalPages
-};
+  return {
+    allBusinesses: state.businessReducer.allBusinesses.businesses,
+    totalPages: state.businessReducer.allBusinesses.totalPages
+  };
 };
 
 export default connect(mapStateToProps)(searchBusiness);
