adfd6d38d0fb1b06e07ac211d3dc57018878b8e0
Temitope <tope_olajide@outlook.com>
2019-06-24 19:00:39 +0100

    Update followActions.js

--- client/src/actions/followActions.js
+++ client/src/actions/followActions.js
@@ -1,31 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-import {
-FOLLOW_USER,UNFOLLOW_USER
-  } from './type';
-  const url = 'http://127.0.0.1:5000/api/user/follow'
-/*   const token = localStorage.getItem('token');
-  axios.defaults.headers.common['authorization'] = token;
- */
+import { FOLLOW_USER, UNFOLLOW_USER } from "./type";
+const url = "http://127.0.0.1:5000/api/user/follow";
 export function follow(userId) {
-    return dispatch =>
-        axios.post(`${url}/${userId}`)
-        .then((response) => {
-            const  {isFollowing} = response.data;
-            dispatch({
-                type: FOLLOW_USER,
-                isFollowing
-            });
-        });
+  return dispatch =>
+    axios.post(`${url}/${userId}`).then(response => {
+      const { isFollowing } = response.data;
+      dispatch({
+        type: FOLLOW_USER,
+        isFollowing
+      });
+    });
 }
 export function unfollow(userId) {
-    return dispatch =>
-        axios.delete(`${url}/${userId}`)
-        .then((response) => {
-            const  {isFollowing} = response.data;
-            dispatch({
-                type: UNFOLLOW_USER,
-                isFollowing
-            });
-        });
+  return dispatch =>
+    axios.delete(`${url}/${userId}`).then(response => {
+      const { isFollowing } = response.data;
+      dispatch({
+        type: UNFOLLOW_USER,
+        isFollowing
+      });
+    });
 }
