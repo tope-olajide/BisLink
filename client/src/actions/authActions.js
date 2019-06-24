39eb947f1f604fa67ad0a9076664d9803dd60a77
Temitope <tope_olajide@outlook.com>
2019-06-24 18:59:44 +0100

    Update authActions.js

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,16 +1,10 @@
-import axios from 'axios';
-import jsonwebtoken from 'jsonwebtoken';
-import {
-  SET_CURRENT_USER, PASSWORD_CHANGED
-} from './type';
+import axios from "axios";
+import jsonwebtoken from "jsonwebtoken";
+import { SET_CURRENT_USER, PASSWORD_CHANGED } from "./type";
 
-const url = 'http://127.0.0.1:5000/api/user/';
-const token = localStorage.getItem('token');
-const setHeaderToken = {
-  headers: {
-    authorization: token
-  }
-};
+const url = "http://127.0.0.1:5000/api/user/";
+const token = localStorage.getItem("token");
+
 export function setCurrentUser(userData) {
   return {
     type: SET_CURRENT_USER,
@@ -18,56 +12,50 @@ export function setCurrentUser(userData) {
   };
 }
 export function signUp(userData) {
-  return dispatch => axios.post(`${url}signup`, userData)
-    .then((response) => {
+  return dispatch =>
+    axios.post(`${url}signup`, userData).then(response => {
       const { token } = response.data;
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
       axios.defaults.headers.common.authorization = token;
-      dispatch(setCurrentUser(jsonwebtoken
-        .decode(localStorage.getItem('token'))));
+      dispatch(
+        setCurrentUser(jsonwebtoken.decode(localStorage.getItem("token")))
+      );
     });
 }
 export function signIn(userData) {
-  return dispatch => axios.post(`${url}signin`, userData)
-    .then((response) => {
+  return dispatch =>
+    axios.post(`${url}signin`, userData).then(response => {
       const { token } = response.data;
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
       axios.defaults.headers.common.authorization = token;
-      dispatch(setCurrentUser(jsonwebtoken
-        .decode(localStorage.getItem('token'))));
+      dispatch(
+        setCurrentUser(jsonwebtoken.decode(localStorage.getItem("token")))
+      );
     });
 }
 export function updateProfile(userData) {
-  return dispatch => axios.put(`${url}profile`, userData)
-    .then((response) => {
+  return dispatch =>
+    axios.put(`${url}profile`, userData).then(response => {
       const {
         user: { token }
       } = response.data;
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
       dispatch(setCurrentUser(jsonwebtoken.decode(token)));
     });
 }
 
-
-
-
-
-
-
-
-
 export function changePassword(userData) {
-  return dispatch => axios.put(`${url}changePassword`, userData)
-    .then(() => {
+  return dispatch =>
+    axios.put(`${url}changePassword`, userData).then(() => {
       dispatch({
         type: PASSWORD_CHANGED
       });
     });
 }
 export function signOut() {
-  return (dispatch) => {
-    localStorage.removeItem('token');
+  return dispatch => {
+    localStorage.removeItem("token");
     dispatch(setCurrentUser({}));
-    window.location = '/auth';
+    window.location = "/auth";
   };
 }
