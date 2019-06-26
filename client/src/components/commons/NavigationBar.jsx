3cb71cfe6518000d3fcdea41f9e1eb8748dfcada
Temitope <tope_olajide@outlook.com>
2019-06-26 04:17:50 +0100

    Update NavigationBar.jsx

--- client/src/components/commons/NavigationBar.jsx
+++ client/src/components/commons/NavigationBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./search.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
 import {
   Navbar,
   NavbarBrand,
