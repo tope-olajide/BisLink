15d268f6896419a456a2942f9c511b13814fa318
Temitope <tope_olajide@outlook.com>
2019-07-05 17:33:36 +0100

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.username,
-    password: process.env.password,
-    database: process.env.database,
-    host: process.env.host,
-    dialect: process.env.dialect
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres'
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL_TEST'
   },
   production: {
     use_env_variable: 'DATABASE_URL'
